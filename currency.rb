82717bd3538dc5153f592383b412fb22e96b1c25
Sarahxdoughty <sarahxdoughty@gmail.com>
2018-07-25 14:25:08 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,3 +1,5 @@
+
+
 def usd_to_eur (dollars)
   return dollars/1.2
 end
@@ -28,4 +30,40 @@ end
 
 def aud_to_usd (audollars)
   return audollars/1.35
+end
+
+puts "How much money do you want to convert?"
+amount = gets.chomp().to_i
+ 
+puts "Pick from the following menu:"
+puts "1. USD to EUR"
+puts "2. USD to JPY"
+puts "3. USD to GBP"
+puts "4. USD to AUD"
+puts "5. EUR to USD"
+puts "6. JPY to USD"
+puts "7. GBP to USD"
+puts "8. AUD to USD"
+ 
+choice = gets.chomp().to_i
+ 
+case choice
+when 1
+  usd_to_eur(amount)
+when 2
+  usd_to_jpy(amount)
+when 3
+  usd_to_gbp(amount)
+when 4
+  usd_to_aud(amount)
+when 5
+  _to_usd(amount)
+when 6
+  #code here
+when 7
+  #code here
+when 8
+  #code here
+else
+  puts "Invalid input, exiting..."
 end
