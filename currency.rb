92c16b43165d01916b284103bbef297fa9e3a4eb
Sarahxdoughty <sarahxdoughty@gmail.com>
2018-07-25 14:06:42 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -6,3 +6,26 @@ def eur_to_usd (euros)
   return euros*1.2
 end
 
+def usd_to_jpy (dollars)
+  return dollars*111.08
+end
+
+def jpy_to_eur (yen)
+  return yen/111.08
+end
+
+def usd_to_gbp (dollars)
+  return dollars*.76
+end
+
+def gbp_to_usd (pounds)
+  return pounds/.76
+end
+
+def usd_to_aud (dollars)
+  return dollars*1.35
+end
+
+def aud_to_usd (audollars)
+  return audollars/135
+end
